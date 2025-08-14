1ac01e85e8f92932cc10a4723d1cbc9801d8627e
sparux99 <maribiamine@gmail.com>
2025-08-14 12:07:42 +0100

    New Init

--- www/js/index.js
+++ www/js/index.js
@@ -1,28 +1,29 @@
-document.addEventListener('deviceready', function() {
-  var permissions = cordova.plugins.permissions;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 
-  var list = [
-    permissions.READ_EXTERNAL_STORAGE,
-    permissions.WRITE_EXTERNAL_STORAGE
-  ];
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+document.addEventListener('deviceready', onDeviceReady, false);
 
-  permissions.hasPermission(list, function(status) {
-    if(status.hasPermission) {
-      console.log("الأذونات متاحة");
-      // هنا تستدعي الدالة التي تحتاج الأذونات
-    } else {
-      permissions.requestPermissions(list, function(status2) {
-        if(status2.hasPermission) {
-          console.log("تم منح الأذونات");
-          // نفذ دوالك بعد الحصول على الأذونات
-        } else {
-          alert("لم يتم منح الأذونات اللازمة");
-        }
-      }, function() {
-        alert("فشل طلب الأذونات");
-      });
-    }
-  }, function() {
-    alert("خطأ في التحقق من الأذونات");
-  });
-});
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    document.getElementById('deviceready').classList.add('ready');
+}
